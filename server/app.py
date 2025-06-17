3aa784a886fa63d884af008f257d730f892e877f
scriptkid-ke <mwangialex813@gmail.com>
2025-06-17 23:01:04 +0300

    Created app.py

---
+++ server/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object("server.config.Config")
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # import models so migrations detect them
+    from server.models import restaurant, pizza, restaurant_pizza
+    # register controllers/blueprints
+    from server.controllers.restaurant_controller import restaurant_bp
+    from server.controllers.pizza_controller import pizza_bp
+    from server.controllers.restaurant_pizza_controller import rp_bp
+
+    app.register_blueprint(restaurant_bp)
+    app.register_blueprint(pizza_bp)
+    app.register_blueprint(rp_bp)
+
+    @app.errorhandler(404)
+    def not_found(e):
+        return jsonify(error="Not found"), 404
+
+    return app
+
+# for flask CLI
+app = create_app()
